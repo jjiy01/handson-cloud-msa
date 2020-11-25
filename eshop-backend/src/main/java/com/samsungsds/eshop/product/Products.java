69d7a6e21566811aaeb7aafe0b85c8b2fcb7e33d
inyong.jang <inyong.jang@samsung.com>
2020-11-25 09:37:21 +0900

    product-service

--- eshop-backend/src/main/java/com/samsungsds/eshop/product/Products.java
+++ eshop-backend/src/main/java/com/samsungsds/eshop/product/Products.java
@@ -1,21 +1,23 @@
 package com.samsungsds.eshop.product;
-
+ 
 import java.util.Arrays;
-
+ 
 public class Products {
     private Product[] products;
-
+ 
     public Product[] getProducts() {
         return products;
     }
-
+ 
+    public Products() {}
+ 
     public Products(Product[] products) {
         this.products = products;
     }
     public void setProducts(Product[] products) {
         this.products = products;
     }
-
+ 
     @Override
     public String toString() {
         return "CartProductResponse{" +
